73a912b7f9993ce7a0fb753661eedf637e902758
baabak aghaei <baabakaghaei@gmail.com>
2023-06-07 23:11:43 +0330

    Add project files.

---
+++ OzhanHr.Domain/Entities/Common/BaseDomain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OzhanHr.Domain.Entities.Common
+{
+    public class BaseDomain
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreateDateComments { get; set; }
+        public DateTime? ModifyDate { get; set; }
+        public string ModifyDateComments { get; set; }
+    }
+}
---
+++ OzhanHr.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OzhanHr.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
